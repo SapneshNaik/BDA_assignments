15b792d98b126b54fa3f400de423d2824bc3974c
Sapnesh Naik <sapnesh@kerneldev.com>
2019-01-07 19:15:32 +0530

    improved structure

---
+++ lab1/1/prog_1.py
@@ -0,0 +1,65 @@
+#1. Read the ‘sample.txt’ file and extract the phone numbers and email ids. Extracted information need to be stored in ‘info.txt’.
+
+import re
+
+
+def main():
+
+	with open('../question_n_source/sample.txt') as input_file:
+
+		#extract all text from file
+		text = input_file.read()
+
+		#extract all email addresses from text
+		emails = re.findall("\S+@\S+", text)  
+
+		#extract all phone numbers with hypyen from text
+		phone_no_hyphen = re.findall("\d{2}-\d{4}-\d{8}", text)
+
+		#extract all phone numbers with brackers from text
+		phone_no_brackets = re.findall("\(\d{2}\)\(\d{4}\)\(\d{8}\)", text)
+
+		#extract all phone numbers with spaces from text
+		phone_no_spaces = re.findall("\d{2} \d{4} \d{8}", text)
+	  
+
+	# print(emails) 
+	# print(phone_no_hyphen) 
+	# print(phone_no_spaces) 
+	# print(phone_no_brackets) 
+
+	with open('info.txt', 'w') as output_file:
+
+		output_file.write("\n--EMAILS--\n")
+
+		#store emails onto info.txt
+		for mail in emails:
+			output_file.write(mail)
+			output_file.write("\n")
+
+
+		#store phone numbers onto info.txt
+
+		output_file.write("\n--PHONE NUMBERS--\n")
+
+		for phone_no in phone_no_spaces:
+			output_file.write(phone_no)
+			output_file.write("\n")
+
+
+		for phone_no in phone_no_hyphen:
+			output_file.write(phone_no)
+			output_file.write("\n")
+
+
+		for phone_no in phone_no_brackets:
+			output_file.write(phone_no)
+			output_file.write("\n")
+
+
+if __name__ == "__main__":
+	main()
+
+
+
+
---
+++ lab1/2/prog_2.py
@@ -0,0 +1,63 @@
+# 2. Read the file ‘state-place.txt’ which is copy pasted from wiki pages. 
+# It starts with state name with constant word [edit] followed by place name with or without college/ university name. 
+# Parse the text file and create a csv file with two columns. First will be state name and second is the place name. 
+# Entries in the csv file will be data extracted from text file.
+
+import re
+import csv
+from collections import OrderedDict
+from itertools import islice, cycle
+
+def shift_dict(dct, shift):
+    shift %= len(dct)
+    return OrderedDict(
+        (k, v)
+        for k, v in zip(dct.keys(), islice(cycle(dct.values()), shift, None))
+    )
+
+
+def main():
+
+    with open('../question_n_source/state-place.txt', encoding="utf8", errors='ignore') as input_file:
+
+        pattern1 = re.compile(".+ \[[eE]dit\]")
+
+
+        #maintain dictinary order
+        states = OrderedDict()
+
+        #Build a dictionary of states and their respective line number occurances
+        lastline = 0
+        for num, line in enumerate(input_file, 1):
+            if pattern1.match(line):
+                # print(line)
+                states.update({line.strip('\n'): (num)})
+                lastline = num
+
+
+
+        #shift states dict values by 1 position so that each
+
+        states_e = shift_dict(states,1)
+
+        input_file.seek(0)
+        with open('prog2_output.csv', mode='w') as csv_file:
+
+            lines = [line.rstrip("\n") for line in input_file]
+            fieldnames = ['state_name', 'place_name']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            writer.writeheader()
+
+            for state, end in zip(states.items(), states_e.values()):
+                    if (end-1) != 0: 
+                        places = lines[state[1]:(end-1)]
+                    else:
+                        places = lines[state[1]:len(lines)]
+                    for place in places:
+                        writer.writerow({'state_name': re.sub(r' \[[eE]dit\]', '', state[0]) , 'place_name': re.sub(r'(\[\d+\])?( \(.+)*', '', place)})
+
+
+    print("Done. Please check prog2_output.csv for result")
+
+if __name__=="__main__":
+    main()
